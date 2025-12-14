9deabd5144fad39463593e56e49fab8949f72eac
Piotr <piotrusk8@gmail.com>
2025-12-14 12:05:15 +0100

    lepiej

--- src/App.js
+++ src/App.js
@@ -3,32 +3,37 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-   const [title, setTitle] = useState('Back to the kitchen');
+    const [title, setTitle] = useState('Back to the kitchen');
 
-   function handleChange(event) {
+    function handleChange(event) {
         setTitle(event.target.value);
     }
+
     let message;
     if (title.length < 5) {
-    message = "Tutuł jest za krótki. Nagrywają takie filmy?";
+        message = "Tutuł jest za krótki. Nagrywają takie filmy?";
     } else if (title.length < 15) {
-    message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
+        message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
     } else {
-    message = "Tytuł niczym z inwazji jakotakich na takowych.";
+        message = "Tytuł niczym z inwazji jakotakich na takowych.";
+
     }
+
 // ...
-{
-    title.length > 0 && <div>{message}</div>
-}
+    {
+        title.length > 0 && <div>{message}</div>
+    }
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>Movie to watch today</h2>
             <div>{message}</div>
-           <input type="text" value={title} onChange={handleChange}/>
-        </div>
-    );
-}
+            <input type="text" value={title} onChange={handleChange}/>
+            <button type="button" onClick={() => alert(title)}>
+                Pokaż tytuł filmu
+            </button>
+        </div>);
 
+}
 export default App;
