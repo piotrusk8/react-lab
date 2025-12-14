17e01a86fa6351013b82464659ca9abcdd6af300
Piotr <piotrusk8@gmail.com>
2025-12-14 11:46:24 +0100

    JAKOTAKO

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let title = 'Katoshi Kamikaze';
+   const [title, setTitle] = useState('Back to the kitchen');
 
-    function handleChange(event) {
-        console.log(event.target.value);
+   function handleChange(event) {
+        setTitle(event.target.value);
     }
+    let message;
+    if (title.length < 5) {
+    message = "Tutuł jest za krótki. Nagrywają takie filmy?";
+    } else if (title.length < 15) {
+    message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
+    } else {
+    message = "Tytuł niczym z inwazji jakotakich na takowych.";
+    }
+// ...
+{
+    title.length > 0 && <div>{message}</div>
+}
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
-            <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Movie to watch today</h2>
+            <div>{message}</div>
+           <input type="text" value={title} onChange={handleChange}/>
         </div>
     );
 }
